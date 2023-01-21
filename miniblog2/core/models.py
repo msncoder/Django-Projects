d84d2361e6ac7a907832df9f050c10d82b8d1ecf
msncoder <msaadnaseem2000@gmail.com>
2023-01-21 20:42:28 +0500

    active logic addit

--- miniblog2/core/models.py
+++ miniblog2/core/models.py
@@ -1,14 +1,15 @@
 from ctypes import addressof
 from email import message
 from django.db import models
+from tinymce import models as tinymce_models
 
 # Create your models here.
 class Core_Home(models.Model):
     title = models.CharField(max_length=70)
-    desc = models.TextField()
+    desc = tinymce_models.HTMLField()
 
 class Core_About(models.Model):
-    desc = models.TextField()
+    desc = tinymce_models.HTMLField()
 
 class Contact_Form(models.Model):
     name = models.CharField(max_length = 70)
